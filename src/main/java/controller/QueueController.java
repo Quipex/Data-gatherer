9dc16cba009e07efa41eda0a2f2df04da27e079e
Quipex <rudas72@gmail.com>
2019-06-07 23:20:40 +0300

    Added email client

--- src/main/java/controller/QueueController.java
+++ src/main/java/controller/QueueController.java
@@ -2,11 +2,17 @@ package controller;
 
 import java.time.LocalTime;
 
-public class QueueController extends AbstractScheduledController{
+public class QueueController extends AbstractScheduledController {
+    private static final int SECOND = 1000;
+    private static final int HOUR = 60 * 60 * SECOND;
+    private static final int DAY = 24 * HOUR;
+    private String dataQueue = "";
+    private String commandQueue = "";
 
-    public QueueController(LocalTime startTime, long cycleInterval) {
+    QueueController(LocalTime startTime, long cycleInterval) {
         super(startTime, cycleInterval);
     }
+
 
     @Override
     protected void cycle() {
@@ -14,6 +20,5 @@ public class QueueController extends AbstractScheduledController{
     }
 
     public void checkCommandQueue() {
-
     }
 }
